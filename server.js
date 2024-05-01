bf18f7bb1dbd010a5b00da462d0cf7659b355ae4
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-05-01 23:41:09 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const sms2email = require("./api/sms2email");
+const email2sms = require("./api/email2sms");
+
+const app = express();
+
+// Load environment variables from .env file
+require("dotenv").config();
+
+// Parse JSON request bodies
+app.use(express.json());
+
+// Parse URL-encoded request bodies
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/sms2email", (req, res) => {
+  sms2email(req, res);
+});
+
+app.post("/email2sms", (req, res) => {
+  email2sms(req, res);
+});
+
+app.get("/", (req, res) => {
+  res.send("Successful response.");
+});
+
+app.listen(3000, () => console.log("App is listening on port 3000."));

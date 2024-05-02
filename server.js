86ff1c70bb12390dc15e4a0335faff01004c09a1
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-05-02 17:36:19 +0600

    Extract information from sms and send email

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const sms2email = require("./api/sms2email");
-const email2sms = require("./api/email2sms");
+const express = require('express');
+const sms2email = require('./api/sms2email');
+const email2sms = require('./api/email2sms');
 
 const app = express();
 
 // Load environment variables from .env file
-require("dotenv").config();
+require('dotenv').config();
 
 // Parse JSON request bodies
 app.use(express.json());
@@ -13,21 +13,22 @@ app.use(express.json());
 // Parse URL-encoded request bodies
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/sms2email", (req, res) => {
-  sms2email(req, res);
+// app.post('/sms2email', (req, res) => {
+//     sms2email(req, res);
+// });
+
+app.post('/email2sms', (req, res) => {
+    email2sms(req, res);
 });
 
-app.post("/email2sms", (req, res) => {
-  email2sms(req, res);
+app.get('/', (req, res) => {
+    res.send('Successful response.');
 });
 
-app.get("/", (req, res) => {
-  res.send("Successful response.");
+app.post('/webhook', (req, res) => {
+    console.log('webhook:', req.body);
+    sms2email(req, res);
+    res.send('Email sent');
 });
 
-app.post("/webhook", (req, res) => {
-  console.log(req.body);
-  res.send("Webhook setup");
-});
-
-app.listen(3000, () => console.log("App is listening on port 3000."));
+app.listen(3000, () => console.log('App is listening on port 3000.'));

532b56f00c9231b57aeeba9d18af06b5bd3c6b2b
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-05-02 11:45:24 +0600

    Added route for webhook

--- server.js
+++ server.js
@@ -25,4 +25,8 @@ app.get("/", (req, res) => {
   res.send("Successful response.");
 });
 
+app.post("/webhook", (req, res) => {
+  res.send("Webhook setup");
+});
+
 app.listen(3000, () => console.log("App is listening on port 3000."));
